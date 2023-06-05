a50741d3489f5108568e32ea7a3e502ac74a798d
vitaliiastakhov <vitalastakhov@gmail.com>
2023-06-05 06:34:35 -0700

    minor changes

--- src/pages/mix/ui/navigation/mix-slide-button.tsx
+++ src/pages/mix/ui/navigation/mix-slide-button.tsx
@@ -1,6 +1,8 @@
 import type { Maybe } from 'yup';
 import { Button } from '@/shared/ui/button/button';
 import { Icon } from '@/shared/ui/icons';
+import clsx from 'clsx';
+import { text } from 'stream/consumers';
 
 interface MixSlideButtonProps {
   slug?: Maybe<string>;
@@ -11,13 +13,12 @@ export const MixSlideButton = ({ slug, direction }: MixSlideButtonProps) => {
   return (
     <Button
       sizeVariant='standard'
-      aria-label={`Go to ${
-        direction === 'left' ? 'previous' : 'next'
-      } next mix`}
+      aria-label={`Go to ${direction === 'left' ? 'previous' : 'next'
+        } next mix`}
       href={slug ?? ''}
       disabled={!slug}
       colorVariant='primary'
-      className='stroke-primary max-lg:bg-black max-lg:text-primary lg:stroke-black lg:hover:stroke-primary'
+      className={clsx('stroke-primary max-lg:bg-black max-lg:text-primary lg:stroke-black lg:hover:stroke-primary', { 'lg:stroke-gray-color': !slug })}
     >
       {direction === 'left' && (
         <div className='w-4  rotate-90 md:w-5'>
@@ -28,11 +29,13 @@ export const MixSlideButton = ({ slug, direction }: MixSlideButtonProps) => {
         {direction === 'left' ? 'prev' : 'next'}
       </span>
 
-      {direction === 'right' && (
-        <div className='w-4 -rotate-90 md:w-5'>
-          <Icon.ArrowIcon />
-        </div>
-      )}
-    </Button>
+      {
+        direction === 'right' && (
+          <div className='w-4 -rotate-90 md:w-5'>
+            <Icon.ArrowIcon />
+          </div>
+        )
+      }
+    </Button >
   );
 };
